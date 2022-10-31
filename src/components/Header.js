21227c00e50d294d27423333729cc495a4b6d0e9
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-10-31 17:30:17 +0530

    btn-cmple

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
     <div className="header-wrapper">
         <div className="main-info">
             <h1>web and app development, promotion.</h1>
+            <a href="#" className="btn-main-offer">contact me</a>
         </div>
     </div>
   )
