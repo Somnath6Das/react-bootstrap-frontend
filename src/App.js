61f531fd50c9930787c6792d35299d2b6bb0415b
Somnath6Das <98904596+Somnath6Das@users.noreply.github.com>
2022-10-31 13:13:42 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Header from "./components/Header";
 
 // React Bootstrap Configuration
 import "../node_modules/react-bootstrap/dist/react-bootstrap";
@@ -10,7 +10,10 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 let App = () => {
   return (
+    <>
       <Navbar/>
+      <Header/>
+    </>
   );
 };
 
